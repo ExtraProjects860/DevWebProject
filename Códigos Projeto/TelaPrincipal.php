94fde3ce478a1b7038cc55c5553910aea851ef3d
ExtraProjects860 <projectsgithub860@gmail.com>
2024-05-08 21:37:27 -0300

    Updates

---
+++ Códigos Projeto/TelaPrincipal.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela Principal</title>
+</head>
+<body>
+    <h1>Teste</h1>
+
+    <?php
+        require_once 'Classes/dadosFuncionario.php';
+
+        dadosFuncionarioMain();
+    ?>
+    
+</body>
+</html>
